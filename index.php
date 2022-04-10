730c31473c31d97ab82329f86f4db4bd5a9cb3b4
akshitrawat1234562gmail.com <akshit689121>
2022-04-10 19:08:26 +0530

    delet function

--- index.php
+++ index.php
@@ -116,7 +116,7 @@ section{
 <body >
 
    <section>
-  <!-- Edit Modal -->
+  <!-- Edit new Modal -->
   <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="editModalLabel"
     aria-hidden="true">
     <div class="modal-dialog" role="document">
